83dd31b58f63d39743fecf90bca22b42683f5f6e
Oneth Dikkumbura <140737710+onethdilvan24@users.noreply.github.com>
2025-02-03 13:25:28 +0530

    Cilent Setup

---
+++ Client/src/Components/Navbar.tsx
@@ -0,0 +1,51 @@
+import {
+  Button,
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+import { Link } from "react-router-dom";
+
+
+
+export function Nav() {
+  return (
+    <Navbar fluid rounded className="bg-[#DFF6FF]">
+      <NavbarBrand href="https://flowbite-react.com">
+        <img
+          src="/favicon.svg"
+          className="mr-3 h-6 sm:h-9"
+          alt="Flowbite React Logo"
+        />
+      </NavbarBrand>
+
+      <NavbarCollapse>
+        <NavbarLink href="#" className="text-[#06283D]" active>
+          <Link to="/">
+          HOME</Link>
+        </NavbarLink>
+        <NavbarLink href="#" className="text-[#06283D]">
+          <Link to="alldoctors">
+          ALL DOCTORS
+          </Link>
+        </NavbarLink>
+        <NavbarLink href="#" className="text-[#06283D]">
+          SHOP
+        </NavbarLink>
+        <NavbarLink href="#" className="text-[#06283D]">
+          ABOUT
+        </NavbarLink>
+      </NavbarCollapse>
+      <div className="flex md:order-2">
+        <Link to="login">
+        <Button className="rounded-3xl bg-[#1363DF] text-[#CFF5E7]">
+          Login
+        </Button>
+        </Link>
+        <NavbarToggle />
+      </div>
+    </Navbar>
+  );
+}
